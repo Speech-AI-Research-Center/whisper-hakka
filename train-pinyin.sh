b538480dc30c0d70e40bdceba3b83e9582c1b965
yfliao <yfliao@nycu.edu.tw>
2023-08-03 21:24:03 +0800

    wer ~5.771830597323769

--- train-pinyin.sh
+++ train-pinyin.sh
@@ -1,14 +1,14 @@
-torchrun --nproc_per_node=10 train/fine-tune_on_custom_dataset.py \
---model_name openai/whisper-medium \
+torchrun --nproc_per_node=2 train/fine-tune_on_custom_dataset.py \
+--model_name openai/whisper-base \
 --language en \
 --sampling_rate 16000 \
 --num_proc 4 \
 --train_strategy epoch \
 --learning_rate 6.25e-6 \
 --warmup 1000 \
---train_batchsize 12 \
---eval_batchsize 6 \
---num_epochs 20 \
+--train_batchsize 32 \
+--eval_batchsize 16 \
+--num_epochs 100 \
 --resume_from_ckpt None \
 --output_dir op_dir_epoch-pinyin \
 --train_datasets output_data-pinyin/train  \
