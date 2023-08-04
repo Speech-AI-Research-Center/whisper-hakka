c9b273f0c5e99ce21374471fa9fa5b2adc8ff647
yfliao <yuanfu.liao@gmail.com>
2023-08-04 19:28:24 +0800

    chage model to base

--- train-char.sh
+++ train-char.sh
@@ -1,14 +1,14 @@
 torchrun --nproc_per_node=10 train/fine-tune_on_custom_dataset.py \
---model_name openai/whisper-medium \
---language zh \
+--model_name openai/whisper-base \
+--language en \
 --sampling_rate 16000 \
 --num_proc 4 \
 --train_strategy epoch \
 --learning_rate 6.25e-6 \
 --warmup 1000 \
---train_batchsize 12 \
---eval_batchsize 6 \
---num_epochs 100 \
+--train_batchsize 32 \
+--eval_batchsize 16 \
+--num_epochs 500 \
 --resume_from_ckpt None \
 --output_dir op_dir_epoch-char \
 --train_datasets output_data-char/train  \
